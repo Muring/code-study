010502b92874c961040f1cae1b21a74a7bdb8707
MuRing <11esh@naver.com>
2024-03-10 02:13:04 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -6,14 +6,18 @@ using namespace std;
 
 int solution(vector<int> citations) {
     int answer = 0;
-
+    
+    // 내림차순으로 정렬
     sort(citations.begin(), citations.end(), greater<int>());
     
+    // 앞에서부터 현 인덱스의 값이 인용된 숫자보다 큰지 확인한다.
     for(int i = 0; i < citations.size(); i++){
         if(citations[i] > i)
             answer++;
+        // 작아지는 순간 더 이상 확인할 필요가 없다.
         else
             break;
     }
-    return answer++;
+    
+    return answer;
 }
