36371b195081f8e682a6e58d7a51008b2d38711b
MuRing <11esh@naver.com>
2024-03-19 09:59:38 +0900

    [level 1] Title: 최소직사각형, Time: 1.63 ms, Memory: 5.03 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int solution(vector<vector<int>> sizes) {
     int temp = 0;
-
+    
+    // 배열을 [작은 수, 큰 수] 형식으로 모두 정렬한다.
     for (int idx = 0; idx < sizes.size(); idx++) {
         if (sizes[idx][0] > sizes[idx][1]) {
             temp = sizes[idx][0];
@@ -17,7 +18,8 @@ int solution(vector<vector<int>> sizes) {
 
     int first_largest = 0;
     int second_largest = 0;
-
+    
+    // 정렬한 배열에서 각 왼쪽, 오른쪽에서의 최대 값을 구해 곱한다.
     for (vector<int> pair : sizes) {
         first_largest = max(first_largest, pair[0]);
         second_largest = max(second_largest, pair[1]);
