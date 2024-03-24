d714bf1444c3bcd02ba32d030c0e30c4dfe5f991
MuRing <11esh@naver.com>
2024-03-25 03:17:37 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -8,10 +8,15 @@ using namespace std;
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
 
-    brown = brown / 2 + 2;
+    // 기본적으로 최대 행의 수를 구한다.
+    int maxRow = brown / 2 + 2;
+    // 기본 최소 행 수는 3이다.
     int row = 3, col;
-    for (int idx = row; idx < brown; idx++) {
-        col = brown - idx;
+    
+    for (int idx = row; idx < maxRow; idx++) {
+        // 주어진 행으로 열 수를 구한다.
+        col = maxRow - idx;
+        // 가로 세로 갈색 칸 수를 뺀 남은 칸수가 노란색 면적이면 맞는 가로 세로 크기를 구한 것이다.
         if ((idx - 2) * (col - 2) == yellow) {
             answer.push_back(col);
             answer.push_back(idx);
