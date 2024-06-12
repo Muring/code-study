1745d2a4590c3bd95ac077b4a533b02b63705965
MuRing <11esh@naver.com>
2024-06-12 15:12:01 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.57 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -11,7 +11,24 @@ public class Solution {
             this.weight = weight;
             this.position = 1;
         }
+
+        public void move() {
+            position++;
+        }
     }
+
+    static Queue<Truck> bridgeQueue = new LinkedList<>();    // 다리에 올라오는 트럭을 저장할 큐
+    static int currentBridgeWeight = 0;
+    static int truckIdx = 0;
+    static int answer = 0;
+
+    // 다리에 다음 트럭 올리는 메소드
+    public static void newTruckToBridge(int weight) {
+        bridgeQueue.offer(new Truck(weight));    // 다리에 트럭을 올리고
+        currentBridgeWeight += weight;   // 현재 다리의 무게를 증가시킨다.
+        truckIdx++; // 다음 트럭 지정
+    }
+
     /**
      * 모든 트럭이 다리가 견딜 수 있는 무게를 고려하여 건너는데 걸린 최소 시간 구하기
      * @param bridgeLength 다리 길이
@@ -20,16 +37,9 @@ public class Solution {
      * @return 트럭이 다리를 모두 건너는데 걸린 최소 시간
      */
     public static int solution(int bridgeLength, int bridgeLimitWeight, int[] truckWeightList) {
-        int answer = 0;
-        Queue<Truck> bridgeQueue = new LinkedList<>();    // 다리에 올라오는 트럭을 저장할 큐
-        int currentBridgeWeight = 0;    // 현재 다리의 무게
-
         // 먼저 다리에 첫 트럭 올리기
-        int truckIdx = 0;
-        bridgeQueue.offer(new Truck(truckWeightList[truckIdx]));    // 다리에 트럭을 올리고
-        currentBridgeWeight += truckWeightList[truckIdx];   // 현재 다리의 무게를 증가시킨다.
-        truckIdx++;
-        answer++;
+        newTruckToBridge(truckWeightList[truckIdx]);
+        answer++;   // 초 증가
 
         while(!bridgeQueue.isEmpty()) {
             // 현재 다리의 상태를 파악하여 트럭이 지나갈지, 올라올지를 결정한다.
@@ -40,15 +50,13 @@ public class Solution {
             }
             // 다리 위에 올려져있는 트럭들을 한 칸 전진
             for(Truck truck : bridgeQueue) {
-                truck.position++;
+                truck.move();
             }
-            answer++;
+            answer++;   // 초 증가
 
-            // 만약 남은 트럭이 있고, 다리에 여유 무게가 있고, 다음 트럭의 무게도 감당할 수 있다면
+            // 만약 남은 트럭이 있고, 다리에 여유 무게가 있고, 다음 트럭의 무게도 감당할 수 있다면 새로운 트럭을 올린다.
             if(truckIdx < truckWeightList.length && currentBridgeWeight + truckWeightList[truckIdx] <= bridgeLimitWeight) {
-                bridgeQueue.offer(new Truck(truckWeightList[truckIdx])); // 다리에 새로운 트럭을 올리고
-                currentBridgeWeight += truckWeightList[truckIdx];   // 현재 다리의 무게를 증가시킨다.
-                truckIdx++;
+                newTruckToBridge(truckWeightList[truckIdx]);
             }
         }
         return answer;
