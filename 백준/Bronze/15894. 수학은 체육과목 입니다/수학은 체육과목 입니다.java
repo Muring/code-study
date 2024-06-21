2264a779f639e4a880c11301e7766d0f568965e6
MuRing <11esh@naver.com>
2024-06-21 10:59:08 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
@@ -2,10 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * BOJ 3009 네 번째 점
- * 각 열에서 숫자를 셌을 때, 개수가 홀 수인 수를 반환하면 된다.
- */
 public class Main {
     static BufferedReader br;
 
