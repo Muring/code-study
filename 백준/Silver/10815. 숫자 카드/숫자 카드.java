3c71550ad43f1741eb10538921ae625b05abe524
MuRing <11esh@naver.com>
2024-06-27 10:16:35 +0900

    [Silver V] Title: 숫자 카드, Time: 888 ms, Memory: 179588 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -7,7 +7,7 @@ public class Main {
     static BufferedReader br;
     static StringBuilder sb;
     static StringTokenizer st;
-    static final int IV = 10000000;
+    static final int IV = 10000000; // 10,000,000 상수 독립 변수
 
     /**
      * 카드 배열 등록 메서드
@@ -21,47 +21,27 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int idx = 0; idx < count; idx++) {
             int num = Integer.parseInt(st.nextToken());
-            arr[idx] = num;
-            status[num + IV]++;
+            arr[idx] = num; // 개별 배열에 입력 값 저장
+            status[num + IV]++; // 전체 상태 배열에 해당 수 보유 여부 체크
         }
     }
 
     private static void Solution() throws IOException {
-//        int myCardCount = Integer.parseInt(br.readLine());
-//        int[] myCards = new int[myCardCount];
-//        enList(myCards, myCardCount);
-//
-//        int deckCount = Integer.parseInt(br.readLine());
-//        int[] deckCards = new int[deckCount];
-//        enList(deckCards, deckCount);
-//
-//        int[] result = new int[deckCount];
-//
-//        for (int myCardIdx = 0; myCardIdx < myCardCount; myCardIdx++) {
-//            for (int deckCardIdx = 0; deckCardIdx < deckCount; deckCardIdx++) {
-//                // 카드가 일치하는 경우
-//                if (myCards[myCardIdx] == deckCards[deckCardIdx]) {
-//                    result[deckCardIdx]++;
-//                }
-//            }
-//        }
-//
-//        for (int num : result) {
-//            sb.append(num).append(" ");
-//        }
-
         // -10,000,000 <= count <= 10,000,000이기 때문에 해당 크기만큼 배열 생성
         int[] status = new int[2 * IV + 1];
 
+        // 내 카드 등록
         int myCardCount = Integer.parseInt(br.readLine());
         int[] myCards = new int[myCardCount];
         enList(myCards, status, myCardCount);
 
+        // 덱 카드 등록
         int deckCount = Integer.parseInt(br.readLine());
         int[] deckCards = new int[deckCount];
         enList(deckCards, status, deckCount);
 
         for (int idx = 0; idx < deckCount; idx++) {
+            // 내가 가지고 있고, 덱에도 있다면 카운트가 2가 되어 있을 것이기 때문에 2일 때 결과 저장
             if (status[deckCards[idx] + IV] == 2)
                 sb.append(1).append(" ");
             else sb.append(0).append(" ");
