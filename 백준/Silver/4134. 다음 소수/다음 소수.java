448515d2dc6861614ec512413065ab463c920c9e
MuRing <11esh@naver.com>
2024-07-01 11:32:56 +0900

    [Silver IV] Title: 다음 소수, Time: 352 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -16,7 +16,7 @@ public class Main {
     private static long findPrimeWithSqrt(long num) {
         while (true) {
             long count = 0;
-            for (long idx = 2; idx < Math.sqrt(num); idx++) {
+            for (long idx = 2; idx <= Math.sqrt(num); idx++) {
                 if (num % idx == 0) {
                     count++;
                     break;
@@ -40,8 +40,8 @@ public class Main {
         int laps = Integer.parseInt(br.readLine());
         for (int lap = 0; lap < laps; lap++) {
             long num = Long.parseLong(br.readLine());
-            sb.append(findPrimeWithBigInteger(num)).append("\n");
-//            sb.append(findPrimeWithSqrt(num)).append("\n");
+//            sb.append(findPrimeWithBigInteger(num)).append("\n");
+            sb.append(findPrimeWithSqrt(num)).append("\n");
         }
 
         System.out.println(sb);
