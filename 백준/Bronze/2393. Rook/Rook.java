04c933e7c8c09cc795243e30dee6779d82fabc3a
MuRing <11esh@naver.com>
2024-07-01 13:57:30 +0900

    [Bronze V] Title: Rook, Time: 92 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("""
+                  ___  ___  ___
+                  | |__| |__| |
+                  |           |
+                   \\_________/
+                    \\_______/
+                     |     |
+                     |     |
+                     |     |
+                     |     |
+                     |_____|
+                  __/       \\__
+                 /             \\
+                /_______________\\
+                """);
+    }
+}
