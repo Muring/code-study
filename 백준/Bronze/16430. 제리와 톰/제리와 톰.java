5e28d761f78a19997e1e488d4c779937f53e80c3
MuRing <11esh@naver.com>
2024-07-02 11:15:50 +0900

    [Bronze V] Title: 제리와 톰, Time: 124 ms, Memory: 15984 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        a = b - a;
+        System.out.println(a + " " + b);
+    }
+}
