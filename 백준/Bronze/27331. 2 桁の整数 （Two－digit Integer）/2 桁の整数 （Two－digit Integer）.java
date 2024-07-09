534192fe6fed824fd2cf9ee9137a729a2a4f5d2a
MuRing <11esh@naver.com>
2024-07-09 09:52:41 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 120 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a * 10 + b);
+    }
+}
