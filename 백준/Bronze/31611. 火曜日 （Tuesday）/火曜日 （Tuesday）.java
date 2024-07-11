97454d976aba148200e58c18f4d851a84054c8cf
MuRing <11esh@naver.com>
2024-07-11 11:09:04 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a % 7 == 2 ? 1 : 0);
+    }
+}
