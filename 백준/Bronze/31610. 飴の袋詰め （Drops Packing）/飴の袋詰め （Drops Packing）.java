05a4a04aa5a1521d921d04c14f788454e4ec1db6
MuRing <11esh@naver.com>
2024-07-11 11:06:52 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        System.out.println(a * b + c);
+    }
+}
